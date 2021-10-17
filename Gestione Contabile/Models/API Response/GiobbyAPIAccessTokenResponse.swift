3335a73d7f65bcc96679fa708f50cde74ee5242b
angelosannino <angelo.sannino@gmail.com>
2021-10-17 16:20:12 +0200

    feature: Create Authorization Token Response model

---
+++ Gestione Contabile/Models/API Response/GiobbyAPIAccessTokenResponse.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+struct GiobbyAPIAccessTokenResponse: Decodable {
+    
+    enum CodingKeys: String, CodingKey {
+        case accessToken = "access_token"
+        case expiresIn = "expires_in"
+        case refreshExpiresIn = "refresh_expires_in"
+        case refreshToken = "refresh_token"
+        case tokenType = "token_type"
+        case notBeforePolicy = "not-before-policy"
+        case sessionState = "session_state"
+    }
+    
+    let accessToken: String
+    let expiresIn: Int
+    let refreshExpiresIn: Int
+    let refreshToken: String
+    let tokenType: String
+    let notBeforePolicy: Int
+    let sessionState: String
+    
+}
